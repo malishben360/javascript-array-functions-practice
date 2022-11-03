4ee821e31aa036a32f31804f90d54431718a7675
Elisha Benjamin <malishben360@gmail.com>
2022-11-03 18:01:59 +0100

    Map method

--- worksheet.js
+++ worksheet.js
@@ -1,39 +1,55 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-];
+	{
+		name: 'Luke Skywalker',
+		height: 172,
+		mass: 77,
+		eye_color: 'blue',
+		gender: 'male',
+	},
+	{
+		name: 'Darth Vader',
+		height: 202,
+		mass: 136,
+		eye_color: 'yellow',
+		gender: 'male',
+	},
+	{
+		name: 'Leia Organa',
+		height: 150,
+		mass: 49,
+		eye_color: 'brown',
+		gender: 'female',
+	},
+	{
+		name: 'Anakin Skywalker',
+		height: 188,
+		mass: 84,
+		eye_color: 'blue',
+		gender: 'male',
+	},
+]
 
 //***MAP***
 //1. Get array of all names
+let names = characters.map((character) => {
+	return character.name
+})
+console.log(names)
 //2. Get array of all heights
+let heights = characters.map((character) => {
+	return character.height
+})
+console.log(heights)
 //3. Get array of objects with just name and height properties
+let nameHeights = characters.map((character) => {
+	return { name: character.name, height: character.height }
+})
+console.log(nameHeights)
 //4. Get array of all first names
+let firstNames = characters.map((character) => {
+	return character.name.split(' ')[0]
+})
+console.log(firstNames)
 
 //***REDUCE***
 //1. Get total mass of all characters
